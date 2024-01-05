5d3bc7fde7c0e71350f0fba69a99e3e00ce95ccc
samuorsini <samuelorsini12@gmail.com>
2024-01-05 14:22:46 +0100

    GameTicTacToe in React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TicTacToe from './components/tictactoe/ticTacToe.jsx';
+
+function App() {
+  return (
+    <div>
+      
+      <TicTacToe />
+
+    </div>
+  );
+}
+
+export default App;
